56e4aa0c7ce50c1fdfb35560cfdd3585ebdbffb2
Piyush <bhangalepiyush@gmail.com>
2019-04-22 22:14:05 +0530

    Uh

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    playlist: "https://www.youtube.com/playlist?list=PL_C8WudqZkOVjPtQ1_f_0eHcMfnXygv5k" // Enter The URL Of The Playlist You Wanna Download
+    playlist: "" // Enter The URL Of The Playlist You Wanna Download
 
 };
