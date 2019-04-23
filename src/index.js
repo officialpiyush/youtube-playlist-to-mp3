d93a89e7c67d51006cc4be20028751e1c7b2bf3b
Piyush <bhangalepiyush@gmail.com>
2019-04-23 18:08:32 +0530

    Remove unecessary eslint ignores

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
-/* eslint-disable no-console */
 const playlistLoader = require("youtube-playlist");
 const fs = require("fs");
 const cmd = require("node-cmd");
 const config = require("../config");
 
-// eslint-disable-next-line prefer-const
-let names = [],
-    i,
-    urls = []; // eslint-disable-line prefer-const
+const names = [],
+    urls = [];
+let i;
 
 getPlaylist(config.playlist);
 
 function fsThings() {
     if (!fs.existsSync("./mp3")) {
         fs.mkdirSync("./mp3");
-        console.log("Folder Created!");
     }
     if (!fs.existsSync("./dl.sh")) {
         fs.writeFileSync("dl.sh", "", err => console.error(err)); //eslint-disable-line
         cmd.run("chmod 777 ./dl.sh");
-        console.log("File created!");
     }
 
     const file = fs.createWriteStream("dl.sh", { flags: "a" });
     for (i = 0; i < urls.length; i++) {
         file.write(
-            `\nytdl ${urls[i]} | ffmpeg -i pipe:0 -b:a 192K -vn ./mp3/"${names[i]}.mp3"`
+            `\nytdl ${urls[i]} | ffmpeg -i pipe:0 -b:a 192K -vn ./mp3/"${
+                names[i]
+            }.mp3"`
         );
-        console.log(`Wrote "${names[i]}"`);
     }
 }
 
@@ -43,8 +40,8 @@ async function getPlaylist(url) {
             names.push(element);
         })
     );
-    
-    await names.forEach(name => name.replace("\"" , ""));
+
+    await names.forEach(name => name.replace("\"", ""));
 
     await fsThings();
 }
