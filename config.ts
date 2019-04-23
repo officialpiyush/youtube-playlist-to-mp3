2a303e01af8196f8fd3a263964ff13929d15a989
Piyush <bhangalepiyush@gmail.com>
2019-04-23 19:52:52 +0530

    TS Rewrite Done :tada:

---
+++ config.ts
@@ -0,0 +1,3 @@
+var playlist = ""; //Enter Your playlist link here
+
+export default playlist;
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import * as playlistloader from "youtube-playlist";
+import * as fs from "fs";
+import * as cmd from "node-cmd";
+import playlist from "../config"
+
+var names: String[] = [], urls: String[] = [], i;
+
+main(playlist);
+
+async function main(url: String) {
+    await playlistloader(url, "url").then((res: any) => {
+        res.data.playlist.forEach((element: String) => {
+            urls.push(element);
+        });
+    });
+
+    await playlistloader(url, "name").then((res: any) => {
+        res.data.playlist.forEach((element: any) => {
+            names.push(element);
+        });
+    });
+
+    await names.forEach(function (name: String) {
+        name.replace("\"", "");
+    });
+
+    var file = fs.createWriteStream("dl.sh", { flags: "a" });
+    for(i=0;i<urls.length;i++) {
+        file.write(
+            `ytdl ${urls[i]} | ffmpeg -i pipe:0 -b:a 192K -vn ./mp3/"${
+                names[i]
+            }.mp3"\n`
+        );
+    }
+    await file.write("chmod -R 777 ./mp3")
+
+    await cmd.run("chmod +x ./dl.sh");
+    console.log("[ PROCESS ] Finished Writing to dl.sh");
+}
